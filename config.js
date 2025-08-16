2a86fd321b8277b54eb748752b4bb24c41622d61
Arnickseven7 <arnickilunga2007@gmail.com>
2025-08-16 13:41:46 +0200

    config.js
    Arnick Seven

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~6lw22Agb#WthFcnhyYrkgFHSiwQFqWxK7z6MHxpJJszdzEFKQH7A",
+SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~ewBViCwQ#aevVaKC9vkKTBboqx-13KqpKUmtLWGE2Qj-6O1hP190",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
