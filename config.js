78de26dfd6bcd26a8019b0b76100813bef29eded
Arnickseven7 <arnickilunga2007@gmail.com>
2025-08-20 11:12:56 +0100

    config.js
    Arnick seven

--- config.js
+++ config.js
@@ -4,86 +4,67 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~qlxSWBhD#gQf3AU4xkCn5NhDxeJn71OLKKYqN9bOYw6KN09yuRIA",
-// add your Ses
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
-// make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-// make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-//anticall on/off
-   ANTI_CALL: process.env.ANTI_CALL || "true",
-// make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY PRINCE XTREME 🤍*",
-// set the auto reply massage on status reply  
-ANTI_DELETE: process.env.ANTI_DELETE || "true",
-// set true false for anti delete     
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
-// change it to 'same' if you want to resend deleted message in same chat     
-WELCOME: process.env.WELCOME || "false",
-// true if want welcome and goodbye msg in groups    
-ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
-// make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "false",
-// make anti link true,false for groups 
-MENTION_REPLY: process.env.MENTION_REPLY || "false",
-// make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/mry39g.jpg",
-// add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || ".",
+    SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~qlxSWBhD#gQf3AU4xkCn5NhDxeJn71OLKKYqN9bOYw6KN09yuRIA",
     
-    AUTO_BIO: process.env.AUTO_BIO || "false",// Replace with your authorized numbers
+    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+    ANTI_CALL: process.env.ANTI_CALL || "true",
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY MR CHAGRIN 🤍*",
     
-    // Timezone for the auto-bio feature
-    TIME_ZONE: process.env.TIME_ZONE || "Afrique/centrale",  // Change to your preferred timezone
-// add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "𝐗",
-// add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "༄☥🤴🏽᪳𝗝𝐔፝֟𝐒𝚻𝚵🌹᪳𝚸፝֟𝐑𝚰𝚴𝐂𝚵™☘️᪳࿐",
-// type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-// make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
-// chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "true",
-// automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "528145550855",
-// add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "ARNICK",
-// add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘʀɪɴᴄᴇ xᴛʀᴇᴍᴇ*",
-// add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/e6orj5.jpg",
-// add img for alive msg
-ALIVE_MSG: process.env.ALIVE_MSG || "_ʜɪ ᴅᴇᴀʀ👋 ɪᴍ xᴛʀᴇᴍᴇ xᴍᴅ ᴄʜᴜᴅᴅʏ ʙᴏᴜᴅʏ🦋 ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ғᴏʀ ᴀsɪsᴛ ʏᴏᴜ._",
-// add alive msg here 
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
-// Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
-// make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
-// false or true for anti bad words  
-MODE: process.env.MODE || "public",
-// make bot public-private-inbox-group 
-ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
-// make anti link true,false for groups 
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-// make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
-// make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-// maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
-// make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
-// true for automatic show typing   
-READ_CMD: process.env.READ_CMD || "false",
-// true if want mark commands as read 
-DEV: process.env.DEV || "527145550855",
-//replace with your whatsapp number        
-ANTI_VV: process.env.ANTI_VV || "true",
-// true for anti once view 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
-// make it true for auto recoding 
+    ANTI_DELETE: process.env.ANTI_DELETE || "true",
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
+    WELCOME: process.env.WELCOME || "false",
+    ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+    
+    ANTI_LINK: process.env.ANTI_LINK || "true",
+    ANTI_LINK_WARNINGS: process.env.ANTI_LINK_WARNINGS || 5, // 5 avertissements
+    ANTI_LINK_EMOJI: process.env.ANTI_LINK_EMOJI || "⚠️",
+    
+    MENTION_REPLY: process.env.MENTION_REPLY || "true",
+    MENTION_REPLY_TEXT: process.env.MENTION_REPLY_TEXT || "> J'suis là 😁",
+    
+    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/mry39g.jpg",
+    PREFIX: process.env.PREFIX || ".",
+    
+    AUTO_BIO: process.env.AUTO_BIO || "false",
+    TIME_ZONE: process.env.TIME_ZONE || "Afrique/centrale",
+    
+    BOT_NAME: process.env.BOT_NAME || "Mr CHAGRIN",
+    STICKER_NAME: process.env.STICKER_NAME || "༄☥🤴🏽᪳𝗝𝐔፝֟𝐒𝚻𝚵🌹᪳𝚸፝֟𝐑𝚰𝚴𝐂𝚵™☘️᪳࿐",
+    
+    CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+    CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+    
+    DELETE_LINKS: process.env.DELETE_LINKS || "true",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "MR CHAGRIN",
+    OWNER_NAME: process.env.OWNER_NAME || "MR CHAGRIN",
+    DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ MR CHAGRIN*",
+    
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://cdn.pixabay.com/photo/2016/11/29/09/32/emoji-1869790_1280.png",
+    ALIVE_MSG: process.env.ALIVE_MSG || "Bot en ligne ✅",
+    
+    READ_MESSAGE: process.env.READ_MESSAGE || "false",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    ANTI_BAD: process.env.ANTI_BAD || "false",
+    
+    MODE: process.env.MODE || "public",
+    ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+    PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+    AUTO_TYPING: process.env.AUTO_TYPING || "false",
+    READ_CMD: process.env.READ_CMD || "false",
+    
+    DEV: process.env.DEV || "MR CHAGRIN",
+    ANTI_VV: process.env.ANTI_VV || "true",
+    AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
+
+    // Commandes spécifiques
+    TAGALL_ONLY_OWNER: true,  // seul toi peut utiliser tagall
+    GAME_PAUSE_TIME: 10 * 60 * 1000, // 10 minutes
 };
